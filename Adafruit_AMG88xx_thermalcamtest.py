fe4a8c5276d31f8f4a79c8be8c204959511decf5
arch-cl0wn <zyxlon-b@protonmail.com>
2021-12-05 22:11:05 +0530

    Initial Commit

---
+++ Adafruit_AMG88xx_thermalcamtest.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import busio
+import board
+import adafruit_amg88xx
+from firebase import firebase
+
+i2c = busio.I2C(board.SCL, board.SDA)
+amg = adafruit_amg88xx.AMG88XX(i2c)
+sum1,c1,c,finval=0,0,0,0
+
+firebase = firebase.FirebaseApplication('https://kishanknow-8a73c-default-rtdb.asia-southeast1.firebasedatabase.app/', None)
+
+while True:
+    for row in amg.pixels:
+        # Pad to 1 decimal place
+        for temp in row:
+            sum1=sum1+float("{0:.1f}".format(temp))
+            c=c+1
+        finval=finval+sum1
+        sum1=0
+        c1=c1+c
+        c=0
+    res=finval/c1
+    print(res)
+    result = firebase.post('/temperature', res)
+    result2=firebase.get('/farm_temperature',None)
+    diff=result2-res
+    result3=firebase.post('/Absolute_Temperature',diff)
+    time.sleep(1)
