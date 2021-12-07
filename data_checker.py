6c8e021fb000e1c9cfe03afd4c7bc907a1c02a0a
arch-cl0wn <zyxlon-b@protonmail.com>
2021-12-08 05:14:40 +0530

    IFTTT Message Done

--- data_checker.py
+++ data_checker.py
@@ -1,4 +1,5 @@
 import Adafruit_AMG88xx_thermalcamtest as Adafruit
+import requests
 
 with open('/home/pi/Adafruit_CircuitPython_AMG88xx/examples/data.txt','r') as f:
     l=f.readlines()
@@ -6,4 +7,5 @@ with open('/home/pi/Adafruit_CircuitPython_AMG88xx/examples/data.txt','r') as f:
         words=row.strip().split(",")
         if (Adafruit.diff>=float(words[1])) and (Adafruit.diff<=float(words[2])):
             print(words[0])
+            r = requests.post('https://maker.ifttt.com/trigger/bacteria_detection/with/key/FhIUKYtS7ymqOC8ePCE0BN8Le2Zp7Rd4C2UFAzZsQl', params={"value1":"none","value2":"none","value3":"none"})
 
