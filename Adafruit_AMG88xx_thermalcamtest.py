80c50b7e681da1ffead23efbb9475dec775623cc
arch-cl0wn <zyxlon-b@protonmail.com>
2021-12-09 00:24:51 +0530

    Minor Revisions

--- Adafruit_AMG88xx_thermalcamtest.py
+++ Adafruit_AMG88xx_thermalcamtest.py
@@ -5,14 +5,25 @@ import time
 import busio
 import board
 import adafruit_amg88xx
+import requests
 from firebase import firebase
-import data_checker as dc
 
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
 sum1,c1,c,finval=0,0,0,0
 
 firebase = firebase.FirebaseApplication('https://kishanknow-8a73c-default-rtdb.asia-southeast1.firebasedatabase.app/', None)
+
+def call_func():
+    with open('/home/pi/Adafruit_CircuitPython_AMG88xx/examples/data.txt','r') as f:
+        l=f.readlines()
+        for row in l:
+            words=row.strip().split(",")
+            if (diff>=float(words[1])) and (diff<=float(words[2])):
+                print(words[0])
+                r = requests.post('https://maker.ifttt.com/trigger/Microbe_Detection/with/key/FhIUKYtS7ymqOC8ePCE0BN8Le2Zp7Rd4C2UFAzZsQl', params={"value1":words[0],"value2":words[3],"value3":"none"})
+
+
 
 while True:
     for row in amg.pixels:
@@ -30,5 +41,5 @@ while True:
     result2=firebase.get('/farm_temperature',None)
     diff=result2-res
     result3=firebase.post('/Absolute_Temperature',diff)
-    dc.call_func()
+    call_func()
     time.sleep(120)
