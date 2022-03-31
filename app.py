c9511841b61db1b364d85cb7529cffa926c2ded5
arch-cl0wn <zyxlon-b@protonmail.com>
2022-03-31 21:29:22 +0530

    Completed Image Upload

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
-# app.py
-from flask import Flask, render_template, request  # importing the render_template function
-from werkzeug.utils import secure_filename
+from flask import Flask
+
+UPLOAD_FOLDER = 'static/uploads/'
 
 app = Flask(__name__)
-# home route
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-@app.route('/upload')
-def upload_file():
-   return render_template('upload.html')
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file1():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
