4bcf95c578d1c841d4b586047cdfea6542cd320c
arch-cl0wn <zyxlon-b@protonmail.com>
2022-03-31 20:55:02 +0530

    File Upload Complete

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 # app.py
-from flask import Flask, render_template  # importing the render_template function
+from flask import Flask, render_template, request  # importing the render_template function
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 # home route
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
 
-app.run(debug = True) 
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file1():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
