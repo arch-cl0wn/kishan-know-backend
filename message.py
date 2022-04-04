a1c3580a398675f6eb9eba500b1264d44609bc7a
arch-cl0wn <zyxlon-b@protonmail.com>
2022-04-04 18:39:25 +0530

    Design Changs

--- message.py
+++ message.py
@@ -5,15 +5,15 @@ import app
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid=os.environ['TWILIO_ACCOUNT_SID']='AC519fe0d0d60c94e37dcb2bc47a89568e'
-auth_token=os.environ['TWILIO_AUTH_TOKEN']='3ed440c837cc4b9d2550643feb2ca5d2'
+account_sid=os.environ['TWILIO_ACCOUNT_SID']='AC3037433857b9dfde1d63726cc4e7ac56'
+auth_token=os.environ['TWILIO_AUTH_TOKEN']='59466b7faf21019b5986835aa3515a03'
 client = Client(account_sid, auth_token)
 
 message1 = client.messages \
                 .create(
-                     body=app.message,
-                     from_='+15736794918',
-                     to='+917008420369'
+                     body="",
+                     from_='+17652953355',
+                     to='+919003032644'
                  )
 
 print(message1.sid)
